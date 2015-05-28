6a2a125c0080e478d71b0d528b5202860bd6ea20
keyanbagheri <keyanbagheri@gmail.com>
2015-05-27 17:00:35 -0700

    updating awesome superhero features

--- lib/superhero.rb
+++ lib/superhero.rb
@@ -1,6 +1,6 @@
 require 'faker'
 class Superhero
-  @@adjectives = ["mega","super","epic"]
+  @@adjectives = ["mega ","super ","epic ", "awesome ", "deadly ", "lethal ", "powerful ", "instantaneous ", "crazy cool ", "wonderous ", "spectacular ", "ingenious ", "mad skillful ", "extraordinary ", "simple ", "weird ", "instinctual ", "hypothetical ", "insurmountable ", "unavoidable ", "uncontrollable ", "killer ", "wicked-awesome ", "sick-nasty ", "janky ", "new-age ", "never before seen ", "special ", "amazing ", "terrible ", "horrible ", "classy ", "terrific ", "mind-bending ", "jaw-dropping ", "", "", "", ""]
   @@superpowers = %{Adhesion
 Amphibious
 Animal control
@@ -130,16 +130,31 @@ X-ray vision
 @@orientation = ['neutral', 'evil', 'good']
 @@level = ['lawful','neutral','chaotic']
   def self.power
-    "#{@@adjectives.sample} #{@@superpowers.sample.downcase.gsub(/\n/, '')}"
+    "#{@@adjectives.sample}#{@@superpowers.sample.downcase.gsub(/\n/, '')}"
   end
   def self.alignment
-    "#{@@level.sample} #{@@orientation.sample}"
+    "#{@@level.sample}-#{@@orientation.sample}"
   end
   def self.name
     Faker::App.name
   end
+  def self.alias
+    Faker::Name.name
+  end
+  def self.nemesis
+    title = ["Dr. ", "Mr. ", "The ", "Professor ", "He Whom They Call ", "The One Named ", "Super ", "Ultra ", "Mega ", "The Angry ", "The Last ", "Master ", "", "Cable Guy ", "Miss ", "Maestro ", "Misses ", "Madame ", "Sister ", "The Temptress ", "A Punk Named ", "Puny ", "Tiny ", "Babyface ", "Sinister ", "The Evilest ", "Murder Face ", "Bad ", "Bad Boy ", "Pretty Boy ", "A Girl Named ", "Squidface ", "", "Temper ", "Alien ", "The Skilled ", "Top Dog ", "T-Dawg ", "Tough Nut ", "The Irate ", "", "Ballerina ", "Mob Boss ", "Pretty Tony ", "Robot Model 688-", "Robot Model 42-", "A Poodle Named ", "The Secret Trigger Word ", "", "Mamy ", "The Body ", "Medusa ", "Gin and ", "Siracha Sauce ", "Slick Willy ", "Jennifer ", "Tamany ", "Pool Shark ", "Loan Shark ", "Mama ", "", "The Only ", "", "", "Tiny ", "", "Master ", "Dr. ", "Dr. ", "The ", "The ", "The ", "Miss ", "Misses ", "Mega ", "Ultra ", "Super ", "", "", "", "", Faker::Name.title + " ", Faker::Name.title + " ", Faker::Name.title + " ", Faker::Name.prefix + " ", Faker::Name.prefix + " ", Faker::Name.prefix + " ", Faker::Name.prefix + " ", Faker::Name.prefix, Faker::Name.prefix + " ", Faker::Hacker.adjective + " ", Faker::Hacker.adjective + " ", Faker::Hacker.adjective + " ", Faker::Hacker.abbreviation + " "]
+    name = [Faker::App.name, Faker::App.name, Faker::App.name, Faker::App.name, Faker::App.name, Faker::App.name, Faker::App.name, Faker::Name.title, Faker::Name.title, Faker::Name.name, Faker::Name.last_name, Faker::Name.last_name, Faker::Name.last_name, Faker::Name.last_name, Faker::Name.last_name, Faker::Name.first_name, Faker::Name.first_name, Faker::Name.first_name, Faker::Name.first_name, Faker::Name.first_name, Faker::Name.first_name, Faker::App.author, Faker::App.author]
+    title = (title + @@adjectives).flatten
+    title.sample + name.sample
+  end
+  def self.weakness
+    adjectives = ["shiney ", "sharp ", "slimey ", 'little ', 'angry ', 'frightened ', "painful ", "", "", "", "", "", "", "", "", "", "", "", "", "heavy ", "rusty ", "faded ", "a pack of ", "some ", "any type of ", "hairy ", "cute and cudly ", "burning ", "overhyped ", "loud ", "thick ", "overly attached ", "twin ", "simple ", "wet ", "red ", "blue ", "green ", "yellow ", "orange ", "brown ", "happy ", "sad ", "cool ", "hot ", "tattered ", "paper ", "plastic ", "a call from ", "sliced ", "too many ", "the last ", "too few ", "sticky ", "kisses from ", "lengthy ", "ketchup on ", "beautiful ", "perrywinkle ", "kind "]
+    nouns = ["animals", "tacks", "toy cars", "cigarrettes", "cereal", "muffins", "corn", "fish", "monkeys", "bears", "stuffed animals", "cartoons", "weddings", "movie scenes", "books", "people", "songs", "folders", "paper cuts", "kitchen sinks", "newspaper articles", "girls", "boys", "men", "women", "dogs", "cats", "trees", "balls", "liquids", "gasses", "solids", "bushes", "birds", "snakes" ,"wookies", "melodies", "apples", "teens", "gangsters", "reporters", "glasses", "hats", "pizzas", "canolis", "beans", "toddlers", "poodles", "octopi", "spleens", "sweats", "t-shirts", "pants", "hats", "love", "hate", "r&b singers", "royal weddings", "symphonies", "spyglasses", "terraformers", "water-dwellers", "land-walkers", "seedlings", "boulders", "airplanes", "seas", "mountains", "worlds", "pies", "cakes", "drinks", "fluids", "words", "phrases", "eyes", "chairs", "crystals", "gems", "rocks", "vegetables", "salt-water", "mummies", "wolves", "ghosts", "aliens", "rappers", "skeletons", "tissues", "lawyers", "politicians", "pansies", "roses", "trains", "particles", "dust mites", "mosquitos", "hippos"]
+    adjectives = (adjectives + @@adjectives).flatten
+    adjectives.sample + nouns.sample
+  end
   def self.describe
-    name = Faker::App.name
-    "#{name} is #{@@level.sample} #{@@orientation.sample} and their power is #{@@adjectives.sample} #{@@superpowers.sample.downcase.gsub(/\n/, '')}"
+    name = self.name
+    "#{name} (alias: #{self.alias}) is #{self.alignment} and their power is #{self.power} #{@@superpowers.sample.downcase.gsub(/\n/, '')}. #{name}'s weakness is #{self.weakness} and their arch-nemesis is #{self.nemesis}"
   end
 end
--- superhero.gemspec
+++ superhero.gemspec
@@ -1,6 +1,7 @@
 Gem::Specification.new do |s|
   s.name = 'superhero'
   s.author = "Robert Wilkinson"
+  s.contributor = "Keyan Bagheri"
   s.version = '0.1.0'
   s.files = ["lib/superhero.rb"]
   s.summary = "A gem to generate random superhero powers"
